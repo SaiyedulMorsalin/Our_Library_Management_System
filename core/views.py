7c91803c66adb5c25383c417f88dc7f29e906401
Md Saiyedul Morsalin <132201648+SaiyedulMorsalin@users.noreply.github.com>
2024-08-18 21:38:41 +0600

    added cores

--- core/views.py
+++ core/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import TemplateView
+from categories.models import Category
+from books.models import Book
+from accounts.models import UserAccount
 
-# Create your views here.
+
+def HomePage(request, category_slug=None):
+    books = Book.objects.all()[:12]
+
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        books = Book.objects.filter(categories=category)
+    categories = Category.objects.all()
+
+    return render(
+        request,
+        "index.html",
+        {
+            "books": books,
+            "categories": categories,
+        },
+    )
